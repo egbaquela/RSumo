486500a1c858145c1d84bb21c370e939b15e96fe
Enrique Gabriel Baquela <egbaquela@hotmail.com>
2013-04-01 11:13:20 -0300

    Prototipos de clases para modelado de rutas y vehiculos

--- RSumo.R
+++ RSumo.R
@@ -4,9 +4,10 @@
 #
 ###################################################
  
+# Sumo Manager
 setClass("adminRSumo",
   representation(
-    sumoBinPath = "character" # Path to sumo.exe and others
+    sumoBinPath = "character", # Path to sumo.exe and others
     name ="character" # Instance name           
   )
 )
@@ -14,4 +15,97 @@ setClass("adminRSumo",
 # Constructor
 adminRSumo <- function(sumoBinPath, name){
   new("adminRSumo", sumoBinPath = sumoBinPath, name = name)
+}
+
+
+# Class for models
+
+setClass("trafficNodes",
+  representation(
+    name = "character",
+    path = "character",
+    nodes = "data.frame"
+  )
+)
+
+trafficNodes <- function(name, path, nodes){
+  new("trafficNodes", name = name, path = path, nodes = nodes)  
+}
+
+setClass("trafficEdges",
+  representation(
+    name = "character",
+    path = "character",
+    edges = "data.frame"
+  )
+)
+
+trafficEdges <- function(name, path, edges){
+  new("trafficEdges", name = name, path = path, edges = edges)  
+}
+
+setClass("trafficNet",
+  representation(
+    name = "character",
+    path = "character",
+    net = "data.frame",
+    nodes = "trafficNodes",
+    edges = "trafficEdges"
+  )
+)
+
+trafficNet <- function(name, path, net, nodes, edges){
+  new("trafficEdges", name = name, path = path, net=net, nodes=nodes, edges = edges)  
+}
+
+
+setClass("vehicleTypes",
+  representation(
+    name = "character",
+    path = "character",
+    types = "data.frame"
+  )
+)
+
+vehicleTypes <- function(name, path, types){
+  new("vehicleTypes", name = name, path = path, types = types)  
+}
+
+setClass("routes",
+  representation(
+    name = "character",
+    path = "character",
+    routesDefinition = "data.frame"
+  )
+)
+
+routes <- function(name, path, routesDefinition){
+  new("routes", name = name, path = path, routesDefinition = routesDefinition)  
+}
+
+setClass("vehicles",
+  representation(
+    name = "character",
+    path = "character",
+    vehiclesDefinition = "data.frame"
+  )
+)
+
+vehicles <- function(name, path, vehiclesDefinition){
+  new("vehicles", name = name, path = path, vehiclesDefinition = vehiclesDefinition)  
+}
+
+
+setClass("trafficRoutes",
+  representation(
+    name = "character",
+    path = "character",
+    vehicleTypes = "vehicleTypes",
+    routes = "routes",    
+    vehicles = "vehicles"
+  )
+)
+
+trafficNet <- function(name, path, vehicleTypes, routes, vehicles){
+  new("trafficRoutes", name = name, path = path, vehicleTypes=vehicleTypes, routes=routes, vehicles = vehicles)  
 }
