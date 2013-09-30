b4bf6b9aaa3db20e83f12ee9b050369c7247289c
unknown <egbaquela@hotmail.com>
2013-09-30 10:10:08 -0300

    Modelado de trips.

---
+++ trafficTrip.R
@@ -0,0 +1,86 @@
+setClass("trafficTrip",
+         representation(
+           id = "character",
+           #vehicleTypes = "data.frame",
+           trips = "data.frame"
+         ),
+         prototype = list(id=character(),
+                          #vehicleTypes = data.frame(),
+                          trips = data.frame()
+         )
+)
+
+trafficTrip <- function(id){
+  object <- new("trafficTrip", id = id) 
+  
+  #vehicleTypes <- data.frame(c("-"),c(0.0),c(0.0), c(0.0), 
+  #                           c(0.0), c(0.0)) 
+  #names(vehicleTypes) <- c("id", "accel", "decel", "length", 
+  #                         "maxSpeed", "sigma")
+  #object@vehicleTypes <- vehicleTypes 
+  
+  trips <- data.frame(c("-"),c(0.0), c("-"), c("-")) 
+  names(trips) <- c("id", "depart", "from", "to")
+  object@trips <- trips
+  
+  object  
+}
+
+setGeneric("appendTrip", function(object, id, depart, from, to){})
+setMethod("appendTrip", "trafficTrip", 
+          function(object, id, depart, from, to){
+            # Asociarlo a una trafficNet
+            id<-as.character(id)
+            from<-as.character(from)
+            to<-as.character(to)
+            trip <- data.frame(id, depart, from, to)
+            object@trips <- rbind(object@trips,trip)
+            object  
+          }
+)
+
+setGeneric("removeTrip", function(object, tripIndex=NA, idTrip=NA){})
+setMethod("removeTrip", "trafficTrip",
+          function(object, tripIndex=NA, idTrip=NA){
+            if(!is.na(tripIndex)){
+              object@trips <- object@trips[-tripIndex] 
+            }else{
+              object@trips <- object@trips[!(object@trips$id==idTrip)]
+            }
+          }
+)
+
+setGeneric("writeTrafficTripToXML", function(object, path){})
+setMethod("writeTrafficTripToXML", "trafficTrip", 
+          function(object, path){
+            parentXMLNode <- xmlNode("trips")
+            for (i in 1:nrow(object@trips)){
+              childNode <- xmlNode("trip")
+              childNode <- addAttributes(childNode, 
+                                         id = object@trips$id[i],
+                                         depart = object@trips$depart[i],
+                                         from = object@trips$from[i],
+                                         to = object@trips$to[i])
+              parentXMLNode <- addChildren(parentXMLNode, 
+                                           childNode) 
+            }
+            
+            saveXML(parentXMLNode, path)
+          }
+)
+
+setGeneric("generateRandomTrip", function(object, trafficNet, number,
+                                          depart){})
+setMethod("generateRandomTrip", "trafficTrip", 
+          function(object, trafficNet, number, depart){
+            #Elimino todos los trips actuales.
+            removeTrip(object, 1:length(object@trip))
+            #Inicio la generación de los trips aleatorios.
+            nodes <-numNodes(trafficNet)
+            origin <- sample(nodes, number, replace=TRUE)
+            destination <- sample(nodes, number, replace=TRUE)
+            object <- appendTrip(object, 1:number, depart, origin, 
+                                 destination)
+            object
+          }
+)
